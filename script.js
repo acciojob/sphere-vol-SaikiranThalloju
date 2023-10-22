6ca53c79f7ab68c1bfae010779eb643abf8ca706
SaikiranThalloju <139635423+SaikiranThalloju@users.noreply.github.com>
2023-10-22 18:43:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-function volume_sphere() {
-    const radiusInput = document.getElementById("radius");
-    const radius = parseFloat(radiusInput.value);
-
-    if (!isNaN(radius)){
-        const volume = (4/3) * Math.PI * Math.pow(radius, 3);
-
-        const volumeInput = document.getElementById("volume");
-        volumeInput.value = volume.toFixed(2); 
-		// volumeInput.innerText = volumeInput.value;
-    } else {
-        // Handle the case where the radius is not a valid number
-        // alert("Please enter a valid radius.");
-    }
+function volumeSphere(){
+	var radius = document.getElementById("radius").value;
+	radius = Math.abs(radius);
+	var volume = (4/3)*Math.PI*Math.pow(radius,3);
+	volume = volume.toFixed(4);
+	document.getElementById("volume").value = volume;
+	return false;
 }
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById("MyForm").onsubmit = volumeSphere;
